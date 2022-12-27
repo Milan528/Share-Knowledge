d37a7d9e8df26e7551e7672ed9ab52a495678d7e
darjan <darjan.drugarinovic@cubic.com>
2022-12-27 17:07:52 +0100

    created user profile info design

--- front/src/pages/profile/index.jsx
+++ front/src/pages/profile/index.jsx
@@ -4,7 +4,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setError } from './redux/slices';
 import ErrorDialog from '../../components/errorDialog';
 import Loader from '../../components/loader';
-import { ContentContainer, MainContainer } from './styles';
+import {
+  ContentContainer,
+  MainContainer,
+  StyledContainer,
+  StyledH1,
+} from './styles';
 import SideNavbar from './components/sideNavbar';
 import UserInfo from './components/userInfo';
 import Posts from './components/posts';
@@ -13,6 +18,11 @@ import { setProfileView } from './components/sideNavbar/redux/slices';
 import { profileView } from './components/sideNavbar/redux/state';
 import ReportedPosts from './components/reportedPosts';
 import ReportedComments from './components/reportedComments';
+import AccountBoxIcon from '@mui/icons-material/Person';
+import AccessibilityNewIcon from '@mui/icons-material/AccessibilityNew';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import PersonIcon from '@mui/icons-material/Person';
+import { Button } from '@mui/material';
 
 const Profile = () => {
   const { error, loading } = useSelector((state) => state.profile);
@@ -36,9 +46,14 @@ const Profile = () => {
   const viewToRender = (
     <>
       <Navbar />
+
       <MainContainer>
         <SideNavbar />
         <ContentContainer>
+          <StyledContainer>
+            <AccountBoxIcon style={{ height: '100%', width: '45px' }} />
+            <h1>{username}</h1>
+          </StyledContainer>
           <Posts />
           <ReportedPosts />
           <ReportedComments />
--- front/src/pages/profile/styles.js
+++ front/src/pages/profile/styles.js
@@ -16,3 +16,14 @@ export const MainContainer = styled.div`
   display: flex;
   flex: 1;
 `;
+
+export const StyledContainer = styled.div`
+  padding: 5px;
+  border-radius: 5px;
+  box-shadow: 1px 1px 0px #999, 2px 2px 0px #999, 3px 3px 0px #999,
+    4px 4px 0px #999, 5px 5px 0px #999, 6px 6px 0px #999;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  margin-bottom: 20px;
+`;
